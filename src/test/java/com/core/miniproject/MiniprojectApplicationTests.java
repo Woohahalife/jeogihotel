3315e0c70259c0c91cc103ce854d8e8cb119c07d
Geon.C <chlrjs1324@gmail.com>
2024-03-19 17:33:40 +0900

    env : h2 데이터베이스 설정 - h2 database 관련 yml 설정 - h2 database url 설정 - h2 database 테스트 결과 확인

--- src/test/java/com/core/miniproject/MiniprojectApplicationTests.java
+++ src/test/java/com/core/miniproject/MiniprojectApplicationTests.java
@@ -1,6 +1,7 @@
 package com.core.miniproject;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +10,4 @@ class MiniprojectApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
