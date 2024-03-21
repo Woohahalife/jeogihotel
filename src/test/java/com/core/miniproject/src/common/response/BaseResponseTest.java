fdd018331bc30ee3c67744cfb3a3c2a554e8d39e
Geon.C <chlrjs1324@gmail.com>
2024-03-21 16:40:47 +0900

    feat : 공통응답 테스트 클래스 작성

--- src/test/java/com/core/miniproject/src/common/response/BaseResponseTest.java
+++ src/test/java/com/core/miniproject/src/common/response/BaseResponseTest.java
@@ -1,10 +1,13 @@
 package com.core.miniproject.src.common.response;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class BaseResponseTest {
 
     @Test
