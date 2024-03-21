5962ea85e74658743670b556e82273e81bd619f2
Geon.C <chlrjs1324@gmail.com>
2024-03-21 16:43:11 +0900

    feat : 보안 설정에 따라 테스트 적용할 수 있는 API KEY 메서드 문서 추가, resolver 및 cors 이슈 해결을 위한 MvcConfig 클래스 작성

--- src/main/java/com/core/miniproject/src/common/config/SwaggerConfig.java
+++ src/main/java/com/core/miniproject/src/common/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.core.miniproject.src.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -27,5 +31,24 @@ public class SwaggerConfig {
                         .group("user")
                         .pathsToMatch(paths[0] + "/**")
                         .build();
+        }
+
+        /**
+         * Swagger OpenAPI에 보안 요소 추가
+         * 발급받은 JWT토큰을 인증에 활용해 API 테스트를 하기 위함
+         */
+        @Bean
+        public OpenAPI apiKey() {
+                SecurityScheme apiKey = new SecurityScheme() // API Key 정의
+                        .type(SecurityScheme.Type.APIKEY) //API Key정의
+                        .in(SecurityScheme.In.HEADER) // 헤더에 위치
+                        .name("Authorization"); // 이름은 Authorization
+
+                SecurityRequirement securityRequirement = new SecurityRequirement() // 보안 요구사항 정의
+                        .addList("Bearer Token"); // Bearer Token 보안 요구사항 추가(모달창에 보임)
+
+                return new OpenAPI()
+                        .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                        .addSecurityItem(securityRequirement);
         }
 }
