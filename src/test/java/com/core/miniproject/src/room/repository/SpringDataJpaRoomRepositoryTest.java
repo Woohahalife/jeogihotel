3909f11a0e1002a5d087b14c3df0969641657454
Geon.C <chlrjs1324@gmail.com>
2024-03-24 22:54:29 +0900

    refactor : SpringDataJpaRoomRepositoryTest.java 수정 - 메서드명 조정 어노테이션 추가

--- src/test/java/com/core/miniproject/src/room/repository/SpringDataJpaRoomRepositoryTest.java
+++ src/test/java/com/core/miniproject/src/room/repository/SpringDataJpaRoomRepositoryTest.java
@@ -7,12 +7,16 @@ import com.core.miniproject.src.room.domain.entity.Room;
 import com.core.miniproject.src.roomprice.domain.RoomPrice;
 import com.core.miniproject.src.roomprice.repository.SpringDataJpaRoomPriceRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.List;
 
 @DataJpaTest
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class SpringDataJpaRoomRepositoryTest {
 
     @Autowired
