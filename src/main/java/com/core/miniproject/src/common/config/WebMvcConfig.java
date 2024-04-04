fe82cdf02cf02991d9b2beb81d89547d9028b823
Geon.C <chlrjs1324@gmail.com>
2024-04-05 02:04:24 +0900

    배포 뷰 레이어와 연결

--- src/main/java/com/core/miniproject/src/common/config/WebMvcConfig.java
+++ src/main/java/com/core/miniproject/src/common/config/WebMvcConfig.java
@@ -19,7 +19,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
-                .allowedOrigins("http://localhost:5173") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+                .allowedOrigins("http://15.164.19.60") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true); // cors를 허용할 method
