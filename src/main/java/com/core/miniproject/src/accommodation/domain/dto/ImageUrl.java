02f629c86a25718abab211cb9fb9ce028d2f455b
Geon.C <chlrjs1324@gmail.com>
2024-04-10 00:01:14 +0900

    feat : S3 이미지 업로드 구현

---
+++ src/main/java/com/core/miniproject/src/accommodation/domain/dto/ImageUrl.java
@@ -0,0 +1,13 @@
+package com.core.miniproject.src.accommodation.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ImageUrl {
+
+    private String imageUrl;
+}
